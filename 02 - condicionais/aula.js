2be348355c1b7a4147c652554cf8a5077af92992
Robson Santana <www.robson.dev@gmail.com>
2024-12-04 00:47:02 -0300

    Hoje aprendemos sobre Condicionais booleam, e alguns sinais

---
+++ 02 - condicionais/aula.js
@@ -0,0 +1,15 @@
+// Condionais Booleam ( verdadeiro / falso)
+// true = verdadeiro
+// false = falso
+
+// Podemos utilizar sinais para nos ajudar nessa condição
+// Ex: < > => <= 
+
+//Encontre numeros pares e impas (Pegue o numero e divida por 2 se o resto da divisão (%) for === 0, o numero é par)
+
+//EX:
+
+const numero =  5;
+const eNumeroPar = numero % 2 === 0;
+
+console.log(eNumeroPar);
